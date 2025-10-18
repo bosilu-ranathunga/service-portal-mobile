567970f2f5dfbc50ad051fed567f6dafd95d27fb
Bosilu Ranathunga <rabjinajith@gmail.com>
2025-10-19 01:21:43 +0530

    pwa update

--- src/App.jsx
+++ src/App.jsx
@@ -34,21 +34,21 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-          {showInstall && (
-            <button
-              style={{ marginTop: '1em' }}
-              onClick={async () => {
-                if (deferredPrompt) {
-                  deferredPrompt.prompt();
-                  const { outcome } = await deferredPrompt.userChoice;
-                  setShowInstall(false);
-                  setDeferredPrompt(null);
-                }
-              }}
-            >
-              Install App
-            </button>
-          )}
+        {showInstall && (
+          <button
+            style={{ marginTop: '1em' }}
+            onClick={async () => {
+              if (deferredPrompt) {
+                deferredPrompt.prompt();
+                const { outcome } = await deferredPrompt.userChoice;
+                setShowInstall(false);
+                setDeferredPrompt(null);
+              }
+            }}
+          >
+            Install App
+          </button>
+        )}
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
