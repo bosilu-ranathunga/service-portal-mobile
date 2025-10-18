1a609c38fdb663d3be4ed72f3f88bc1aa1b5ba62
Bosilu Ranathunga <rabjinajith@gmail.com>
2025-10-19 01:45:20 +0530

    pwa

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    port: 3001, // 👈 change this to your preferred port
-  },
 })
